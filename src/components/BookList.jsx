0940c7bf0d2dabcbe86a9f975b73d06434956d3e
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-01-13 16:35:36 +0100

    compenetDidUpdate aggiunto

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,41 +1,63 @@
-import { Component } from 'react'
-import SingleBook from './SingleBook'
-import { Col, Form, Row } from 'react-bootstrap'
+import { Component } from "react";
+import SingleBook from "./SingleBook";
+import { Col, Form, Row } from "react-bootstrap";
+import CommentArea from "./CommentArea";
 
 class BookList extends Component {
   state = {
-    searchQuery: '',
-  }
+    searchQuery: "",
+    selectedAsin: "",
+  };
+
+  bookListState = (asin) => {
+    this.setState({
+      selectedAsin: asin,
+    });
+  };
 
   render() {
     return (
       <>
-        <Row className="justify-content-center mt-5">
-          <Col xs={12} md={4} className="text-center">
-            <Form.Group>
-              <Form.Control
-                type="search"
-                placeholder="Cerca un libro"
-                value={this.state.searchQuery}
-                onChange={(e) => this.setState({ searchQuery: e.target.value })}
-              />
-            </Form.Group>
+        <Row>
+          <Col xs={8} className=" border-end border-2 border-black">
+            <Row className="justify-content-center mt-5">
+              <Col xs={12} md={4} className="text-center">
+                <Form.Group>
+                  <Form.Control
+                    type="search"
+                    placeholder="Cerca un libro"
+                    value={this.state.searchQuery}
+                    onChange={(e) =>
+                      this.setState({ searchQuery: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="g-2 mt-3">
+              {this.props.books
+                .filter((b) =>
+                  b.title.toLowerCase().includes(this.state.searchQuery)
+                )
+                .map((b) => (
+                  <Col xs={12} md={4} key={b.asin}>
+                    <SingleBook book={b} bookListState={this.bookListState} />
+                  </Col>
+                ))}
+            </Row>
+          </Col>
+          <Col>
+            {this.state.selectedAsin == "" ? (
+              console.log("ciao")
+            ) : (
+              <CommentArea asin={this.state.selectedAsin} />
+            )}
+            {console.log("renderizzandu", this.state.selectedAsin)}
           </Col>
         </Row>
-        <Row className="g-2 mt-3">
-          {this.props.books
-            .filter((b) =>
-              b.title.toLowerCase().includes(this.state.searchQuery)
-            )
-            .map((b) => (
-              <Col xs={12} md={4} key={b.asin}>
-                <SingleBook book={b} />
-              </Col>
-            ))}
-        </Row>
       </>
-    )
+    );
   }
 }
 
-export default BookList
+export default BookList;
--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -36,6 +36,37 @@ class CommentArea extends Component {
     }
   };
 
+  componentDidUpdate = async (prevProps) => {
+    if (prevProps.asin !== this.props.asin) {
+      try {
+        let response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/comments/" +
+            this.props.asin,
+          {
+            headers: {
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVjMDAwNWQyMjA3MTAwMTVkZTJmNTQiLCJpYXQiOjE3MzY3NzYxMjgsImV4cCI6MTczNzk4NTcyOH0.0p9MF4Q9j3h3YMpsqz8w9PqVX3ikVZlbPJpfLBIsZwE",
+            },
+          }
+        );
+        console.log(response);
+        if (response.ok) {
+          let comments = await response.json();
+          this.setState({
+            comments: comments,
+            isLoading: false,
+            isError: false,
+          });
+        } else {
+          this.setState({ isLoading: false, isError: true });
+        }
+      } catch (error) {
+        console.log(error);
+        this.setState({ isLoading: false, isError: true });
+      }
+    }
+  };
+
   render() {
     return (
       <div className="text-center">
