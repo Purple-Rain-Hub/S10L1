3cbeecd7a62f7a8e8158c8f7d7cf0845bbc93032
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-01-13 15:38:53 +0100

    sistemati alcuni dettagli

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,39 +1,40 @@
-import { Component } from 'react'
-import CommentList from './CommentList'
-import AddComment from './AddComment'
-import Loading from './Loading'
-import Error from './Error'
+import { Component } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
 
 class CommentArea extends Component {
   state = {
     comments: [],
     isLoading: true,
     isError: false,
-  }
+  };
 
   componentDidMount = async () => {
     try {
       let response = await fetch(
-        'https://striveschool-api.herokuapp.com/api/comments/' +
+        "https://striveschool-api.herokuapp.com/api/comments/" +
           this.props.asin,
         {
           headers: {
-            Authorization: 'Bearer inserisci-qui-il-tuo-token',
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVjMDAwNWQyMjA3MTAwMTVkZTJmNTQiLCJpYXQiOjE3MzY3NzYxMjgsImV4cCI6MTczNzk4NTcyOH0.0p9MF4Q9j3h3YMpsqz8w9PqVX3ikVZlbPJpfLBIsZwE",
           },
         }
-      )
-      console.log(response)
+      );
+      console.log(response);
       if (response.ok) {
-        let comments = await response.json()
-        this.setState({ comments: comments, isLoading: false, isError: false })
+        let comments = await response.json();
+        this.setState({ comments: comments, isLoading: false, isError: false });
       } else {
-        this.setState({ isLoading: false, isError: true })
+        this.setState({ isLoading: false, isError: true });
       }
     } catch (error) {
-      console.log(error)
-      this.setState({ isLoading: false, isError: true })
+      console.log(error);
+      this.setState({ isLoading: false, isError: true });
     }
-  }
+  };
 
   render() {
     return (
@@ -43,8 +44,8 @@ class CommentArea extends Component {
         <AddComment asin={this.props.asin} />
         <CommentList commentsToShow={this.state.comments} />
       </div>
-    )
+    );
   }
 }
 
-export default CommentArea
+export default CommentArea;
