8adc977c43ae90f936d49eb80481d1acff376954
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-01-14 15:05:58 +0100

    convertito CommentArea SingleBook a funzione

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,47 +1,25 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import CommentList from "./CommentList";
 import AddComment from "./AddComment";
 import Loading from "./Loading";
 import Error from "./Error";
 
-class CommentArea extends Component {
-  state = {
-    comments: [],
-    isLoading: true,
-    isError: false,
-  };
+const CommentArea = (props) => {
+  // state = {
+  //   comments: [],
+  //   isLoading: true,
+  //   isError: false,
+  // };
 
-  componentDidMount = async () => {
-    try {
-      let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/comments/" +
-          this.props.asin,
-        {
-          headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVjMDAwNWQyMjA3MTAwMTVkZTJmNTQiLCJpYXQiOjE3MzY3NzYxMjgsImV4cCI6MTczNzk4NTcyOH0.0p9MF4Q9j3h3YMpsqz8w9PqVX3ikVZlbPJpfLBIsZwE",
-          },
-        }
-      );
-      console.log(response);
-      if (response.ok) {
-        let comments = await response.json();
-        this.setState({ comments: comments, isLoading: false, isError: false });
-      } else {
-        this.setState({ isLoading: false, isError: true });
-      }
-    } catch (error) {
-      console.log(error);
-      this.setState({ isLoading: false, isError: true });
-    }
-  };
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
-  componentDidUpdate = async (prevProps) => {
-    if (prevProps.asin !== this.props.asin) {
+  useEffect(() => {
+    async () => {
       try {
         let response = await fetch(
-          "https://striveschool-api.herokuapp.com/api/comments/" +
-            this.props.asin,
+          "https://striveschool-api.herokuapp.com/api/comments/" + props.asin,
           {
             headers: {
               Authorization:
@@ -52,31 +30,32 @@ class CommentArea extends Component {
         console.log(response);
         if (response.ok) {
           let comments = await response.json();
-          this.setState({
-            comments: comments,
-            isLoading: false,
-            isError: false,
-          });
+          // this.setState({ comments: comments, isLoading: false, isError: false });
+          setComments(comments);
+          setIsLoading(false);
+          setIsError(false);
         } else {
-          this.setState({ isLoading: false, isError: true });
+          // this.setState({ isLoading: false, isError: true });
+          setIsLoading(false);
+          setIsError(true);
         }
       } catch (error) {
         console.log(error);
-        this.setState({ isLoading: false, isError: true });
+        // this.setState({ isLoading: false, isError: true });
+        setIsLoading(false);
+        setIsError(true);
       }
-    }
-  };
+    };
+  }, [props]);
 
-  render() {
-    return (
-      <div className="text-center">
-        {this.state.isLoading && <Loading />}
-        {this.state.isError && <Error />}
-        <AddComment asin={this.props.asin} />
-        <CommentList commentsToShow={this.state.comments} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="text-center">
+      {isLoading && <Loading />}
+      {isError && <Error />}
+      <AddComment asin={props.asin} />
+      <CommentList commentsToShow={comments} />
+    </div>
+  );
+};
 
 export default CommentArea;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,27 +1,21 @@
-import { Component } from "react";
 import { Card } from "react-bootstrap";
-// import CommentArea from "./CommentArea";
 
-class SingleBook extends Component {
-  render() {
-    return (
-      <>
-        <Card
-          onClick={() => {
-            this.props.bookListState(this.props.book.asin);
-          }}
-          style={{ border: this.props.selected ? "3px solid red" : "none" }}
-        >
-          <Card.Img variant="top" src={this.props.book.img} />
-          <Card.Body>
-            <Card.Title style={{ color: "black" }}>
-              {this.props.book.title}
-            </Card.Title>
-          </Card.Body>
-        </Card>
-      </>
-    );
-  }
-}
+const SingleBook = (props) => {
+  return (
+    <>
+      <Card
+        onClick={() => {
+          props.bookListState(props.book.asin);
+        }}
+        style={{ border: props.selected ? "3px solid red" : "none" }}
+      >
+        <Card.Img variant="top" src={props.book.img} />
+        <Card.Body>
+          <Card.Title style={{ color: "black" }}>{props.book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
 export default SingleBook;
