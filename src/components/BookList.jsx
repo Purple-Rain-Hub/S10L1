b91389a48f060261ff05f1f2993190b7568e14a1
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-01-14 14:44:36 +0100

    S10L2 booklist convertita a funzione

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,82 +1,77 @@
-import { Component } from "react";
 import SingleBook from "./SingleBook";
 import { Col, Form, Row } from "react-bootstrap";
 import CommentArea from "./CommentArea";
+import { useState } from "react";
 
-class BookList extends Component {
-  state = {
-    searchQuery: "",
-    selectedAsin: "",
+const BookList = (props) => {
+  // state = {
+  //   searchQuery: "",
+  //   selectedAsin: "",
+  // };
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedAsin, setSelectedAsin] = useState("");
+
+  const bookListState = (asin) => {
+    // this.setState({
+    //   selectedAsin: asin,
+    // });
+    setSelectedAsin(asin);
   };
 
-  bookListState = (asin) => {
-    this.setState({
-      selectedAsin: asin,
-    });
-  };
-
-  showCard = (b) => {
-    if (b.asin == this.state.selectedAsin) {
+  const showCard = (b) => {
+    if (b.asin == selectedAsin) {
       return (
         <Col xs={4} key={b.asin}>
-          <SingleBook
-            book={b}
-            bookListState={this.bookListState}
-            selected={true}
-          />
+          <SingleBook book={b} bookListState={bookListState} selected={true} />
         </Col>
       );
     } else {
       return (
         <Col xs={4} key={b.asin}>
-          <SingleBook
-            book={b}
-            bookListState={this.bookListState}
-            selected={false}
-          />
+          <SingleBook book={b} bookListState={bookListState} selected={false} />
         </Col>
       );
     }
   };
 
-  render() {
-    return (
-      <>
-        <Row>
-          <Col xs={8} className=" border-end border-2 border-black">
-            <Row className="justify-content-center mt-5">
-              <Col xs={12} md={4} className="text-center">
-                <Form.Group>
-                  <Form.Control
-                    type="search"
-                    placeholder="Cerca un libro"
-                    value={this.state.searchQuery}
-                    onChange={(e) =>
-                      this.setState({ searchQuery: e.target.value })
+  return (
+    <>
+      <Row>
+        <Col xs={8} className=" border-end border-2 border-black">
+          <Row className="justify-content-center mt-5">
+            <Col xs={12} md={4} className="text-center">
+              <Form.Group>
+                <Form.Control
+                  type="search"
+                  placeholder="Cerca un libro"
+                  value={searchQuery}
+                  onChange={(e) =>
+                    // this.setState({ searchQuery: e.target.value })
+                    {
+                      setSearchQuery(e.target.value);
                     }
-                  />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row className="g-2 mt-3">
-              {this.props.books
-                .filter((b) =>
-                  b.title.toLowerCase().includes(this.state.searchQuery)
-                )
-                .map((b) => this.showCard(b))}
-            </Row>
-          </Col>
-          <Col>
-            {this.state.selectedAsin == "" ? (
-              console.log("ciao")
-            ) : (
-              <CommentArea asin={this.state.selectedAsin} />
-            )}
-          </Col>
-        </Row>
-      </>
-    );
-  }
-}
+                  }
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="g-2 mt-3">
+            {props.books
+              .filter((b) => b.title.toLowerCase().includes(searchQuery))
+              .map((b) => showCard(b))}
+          </Row>
+        </Col>
+        <Col>
+          {selectedAsin == "" ? (
+            console.log("ciao")
+          ) : (
+            <CommentArea asin={selectedAsin} />
+          )}
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default BookList;
